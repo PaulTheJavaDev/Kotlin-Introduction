5b7b7a401e4f682d394f4b05a5c8c42ff45c71e0
Paul <Paul-Ludwig.Simon@t-online.de>
2025-02-02 15:20:12 +0100

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main() {
+
+    val scanner = Scanner(System.`in`);
+    
+    println("What is your age?")
+    
+    val age = scanner.nextInt()
+
+    if (age != 1) {
+        println("You are $age years old!")
+    } else {
+        println("You are $age year old!")
+    }
+}
